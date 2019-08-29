d651646bf850e8137d0b43eb2d3e51e7b6b1f759
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-08-29 18:34:06 -0500

    Fixed saber preview not displaying when first entering the menu.

--- CustomSaber/UI/SaberListViewController.cs
+++ CustomSaber/UI/SaberListViewController.cs
@@ -20,13 +20,7 @@ namespace CustomSaber
 
         public override void __Activate(ActivationType activationType)
         {
-            for (var i = 0; i < SaberLoader.AllSabers.Count; i++)
-            {
-                if (SaberLoader.AllSabers[i].Path == Plugin._currentSaberName)
-                {
-                    selected = i;
-                }
-            }
+            selected = SaberLoader.FindSaberByName(Plugin._currentSaberName);
 
             base.__Activate(activationType);
             _customListTableView.SelectCellWithIdx(selected);
@@ -71,8 +65,8 @@ namespace CustomSaber
             _versionNumber.color = Color.white;
 
             _customListTableView.didSelectCellWithIdxEvent += _sabersTableView_DidSelectRowEvent;
-            _customListTableView.ScrollToCellWithIdx(SaberLoader.FindSaberByName(Plugin._currentSaberName), TableViewScroller.ScrollPositionType.Beginning, false);
-            _customListTableView.SelectCellWithIdx(SaberLoader.FindSaberByName(Plugin._currentSaberName), false);
+            _customListTableView.ScrollToCellWithIdx(selected, TableViewScroller.ScrollPositionType.Beginning, false);
+            _customListTableView.SelectCellWithIdx(selected, false);
         }
 
         protected override void DidDeactivate(DeactivationType type) => base.DidDeactivate(type);
